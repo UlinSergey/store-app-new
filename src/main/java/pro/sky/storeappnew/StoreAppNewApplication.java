39ce9decf074520c60b74ec9c50155b2f7a0e4a9
Sergey Ulin <PYLIN2002@yandex.ru>
2024-03-27 09:32:42 +0300

    Initial commit

---
+++ src/main/java/pro/sky/storeappnew/StoreAppNewApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.storeappnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreAppNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreAppNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/storeappnew/StoreAppNewApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.storeappnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreAppNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
